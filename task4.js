3bd48b58d9d7ea469c75de48dcb567fc46488bd1
suraj Adhikari <suraj.adhikari@gmail.com>
2026-07-21 19:10:32 +0530

    Task4 calculate age difference

---
+++ task4.js
@@ -0,0 +1,10 @@
+// Task 4: Calculate the age difference between two patients
+let patient1Age = 54;
+let patient2Age = 21;
+
+let ageDifference = Math.abs(patient1Age - patient2Age);
+
+console.log(`Patient 1 Age: ${patient1Age}`);
+console.log(`Patient 2 Age: ${patient2Age}`);
+console.log(`Age Difference: ${ageDifference} years`);
+
