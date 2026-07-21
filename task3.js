1c342fa78b6989ddc0a324e32a489a9be4b6993d
suraj Adhikari <suraj.adhikari@gmail.com>
2026-07-21 19:08:32 +0530

    Add task3.js to declare variables and print their datatypes

---
+++ task3.js
@@ -0,0 +1,8 @@
+// Task 3: Declare variables (Number, String, Boolean) and print their datatypes
+let patientId = 10024;        // Number
+let patientName = "Aastha";    // String
+let isCritical = false;       // Boolean
+
+console.log("patientId type:", typeof patientId);
+console.log("patientName type:", typeof patientName);
+console.log("isCritical type:", typeof isCritical);
