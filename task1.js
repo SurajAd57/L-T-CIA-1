77b325414379ac852c22ab2b5fb2fbf8dade02ec
suraj Adhikari <suraj.adhikari@gmail.com>
2026-07-21 19:00:53 +0530

    Initial commit

---
+++ task1.js
@@ -0,0 +1,4 @@
+// Task 1: Print a welcome message for the Hospital Patient System
+console.log("=========================================");
+console.log("  Welcome to the Hospital Patient System ");
+console.log("=========================================");
