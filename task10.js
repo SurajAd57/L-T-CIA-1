c8e861d68364ec69842637f6564728308b9dd362
suraj Adhikari <suraj.adhikari@gmail.com>
2026-07-21 19:38:16 +0530

    Add task10.js to create a patient object and print its properties

---
+++ task10.js
@@ -0,0 +1,14 @@
+// Task 10: Create a patient object with 5 properties and print individually
+const patient = {
+    id: "P-501",
+    name: "Rahul Sharma",
+    age: 29,
+    bloodGroup: "O+",
+    isAdmitted: true
+};
+
+console.log("Patient ID:", patient.id);
+console.log("Name:", patient.name);
+console.log("Age:", patient.age);
+console.log("Blood Group:", patient.bloodGroup);
+console.log("Admitted Status:", patient.isAdmitted);
