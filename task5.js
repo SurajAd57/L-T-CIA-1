5d94358cb02285b4368c2bccd5b5897f26d4ff9c
suraj Adhikari <suraj.adhikari@gmail.com>
2026-07-21 19:16:12 +0530

    Add task5.js to check patient age condition using if-else

---
+++ task5.js
@@ -0,0 +1,10 @@
+// Task 5: Check patient age condition using if-else
+let patientAge = 68;
+
+if (patientAge >= 60) {
+    console.log("Patient Category: Senior Citizen (Priority Care)");
+} else if (patientAge >= 18) {
+    console.log("Patient Category: Adult");
+} else {
+    console.log("Patient Category: Minor");
+}
